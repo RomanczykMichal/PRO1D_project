aed672d9cbefb5fba776c77a8e22152341968d17
Michal Romanczyk <s20422@pjwstk.edu.pl>
2022-01-07 15:21:00 +0100

    Fixed data prep.

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import numpy as np
 import pandas as pd
+import algorithms as alg
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_regression
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.model_selection import train_test_split
-import algorithms as alg
+
 
 def main():
     # zbiory danych
-    df_original, df_cleaned, df_mean, df_med = prep_data()
+    _, df_cleaned, df_mean, df_med = prep_data()
 
-    # utworzenie zmiennych
-    X, y = prep_dataset(df_cleaned)
-    column_names = future_selector(X, y)
-    # przefiltrowanie kolumn na te najważniejsze, z metody future_names
-    # df_cleaned_filtered = df_cleaned[column_names]
+    # future selector
+    X_temp, y_temp = prep_dataset(df_cleaned)
+    column_names = future_selector(X_temp, y_temp)
+    df_cleaned_filtered = df_cleaned[column_names]
+
+    # inicjacja X i y
+    inputs = df_cleaned_filtered
+    X, y = prep_dataset(inputs)
 
     # podział na zbiory testowe i treningowe
-    # Tutaj można podmienić wartość inputs na dowolny dataset
-    inputs = df_cleaned
-    X_train, X_test, y_train, y_test = train_test_split(inputs.values, y.values, test_size=0.25, random_state=5)
+    X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.25, random_state=5)
     y_train = y_train.ravel()
     y_test = y_test.ravel()
 
@@ -27,13 +29,13 @@ def main():
     alg.neural_network(X_train, X_test, y_train, y_test, X, y)
 
     # Drzewo regresyjne
-    alg.decision_tree_regressor(X_train, X_test, y_train, y_test)
+    alg.decision_tree_regressor(X_train, X_test, y_train, y_test, X, y)
 
     # regresja liniowa
-    alg.linear_regression(X_train, X_test, y_train, y_test)
+    alg.linear_regression(X_train, X_test, y_train, y_test, X, y)
 
     # regresja nieliniowa
-    alg.non_linear_regression(X_train, X_test, y_train, y_test)
+    alg.non_linear_regression(X_train, X_test, y_train, y_test, X, y)
 
 
 def prep_data():
@@ -103,14 +105,16 @@ def future_selector(X, y):
          Sprawdzic czy jest mozliwosc po zrobionym juz fit_transform
 
     TODO ogarnac w ogole co robi fit_transform
+
+    TODO pobawic sie z wartoscia k
     """
 
-    f_selector = SelectKBest(score_func=f_regression, k=80)
+    f_selector = SelectKBest(score_func=f_regression, k=30)
     X_selected = f_selector.fit_transform(X.values, y.values.ravel())
     X_selected_df = pd.DataFrame(X_selected, columns=[X.columns[i] for i in range(len(X.columns)) if
                                                       f_selector.get_support()[i]])
 
-    f_selector2 = SelectKBest(score_func=mutual_info_regression, k=80)
+    f_selector2 = SelectKBest(score_func=mutual_info_regression, k=30)
     X_selected2 = f_selector2.fit_transform(X.values, y.values.ravel())
     X_selected2_df = pd.DataFrame(X_selected2, columns=[X.columns[i] for i in range(len(X.columns)) if
                                                         f_selector2.get_support()[i]])

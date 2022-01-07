da4340d1b36ce00b7ff00f6fc84e00a47b0234ad
Michal Romanczyk <s20422@pjwstk.edu.pl>
2022-01-07 15:20:39 +0100

    Added CV

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,9 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.model_selection import cross_val_score
+from sklearn import tree
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 
 
 def neural_network(X_train, X_test, y_train, y_test, X, y):
@@ -11,38 +14,42 @@ def neural_network(X_train, X_test, y_train, y_test, X, y):
         https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPRegressor.html#sklearn.neural_network.MLPRegressor
         https://scikit-learn.org/stable/modules/neural_networks_supervised.html
 
-        TODO Chyba naprawilem ta siec. podejrzewam, ze problem leżał tutaj w wielkości sieci, alphie i w tym
-             jak dzielilismy dane. Wyrzucało błąd bo do metody train_test_split wrzucalismy DataFrame, a to
-             chcialo ndarray.
-
-        TODO Dla max_iter=1000 wartość się zwiększa do ~0.99
-             Dla max_iter=500  wartosc wynosi ~0.83
-             Trzeba się zastanowić jak bardzo chcemy szkolić naszą sieć, żeby nie była przetrenowana
+        TODO random_state??????
 
         scores - tablica wyników z cross-validation.
         cross_val_model - cv=5 to tyle razy odpalane jest uczenie.
     """
-    model_r = MLPRegressor(random_state=5, hidden_layer_sizes=(50), activation='logistic', alpha=0.0001,
-                           learning_rate_init=0.0001, solver='adam', max_iter=500, early_stopping=True, verbose=True)
+    model_r = MLPRegressor(hidden_layer_sizes=(22,), activation='logistic', alpha=0.0001, learning_rate_init=0.0001,
+                           solver='adam', max_iter=2000, early_stopping=True, verbose=False)
     model_r.fit(X_train, y_train)
-    scores = cross_val_score(model_r, X.values, y.values, cv=5)
-    print('Neural network score:', model_r.score(X_test, y_test))
+    scores = cross_val_score(model_r, X.values, y.values.ravel(), cv=5, scoring='r2')
+    print('\nNeural network score:', model_r.score(X_test, y_test))
     print('Neural network cross-validation mean score is: ', scores.mean(), scores.std())
 
 
-def decision_tree_regressor(X_train, X_test, y_train, y_test):
+def decision_tree_regressor(X_train, X_test, y_train, y_test, X, y):
     """
     Sources:
         https://www.statsoft.pl/textbook/stathome_stat.html?https%3A%2F%2Fwww.statsoft.pl%2Ftextbook%2Fstcart.html
 
         TODO Dziwnie duze wyniki wychodza :)
     """
-    model_r = DecisionTreeRegressor(random_state=0)
-    model_r.fit(X_train, y_train)
-    print('Decision tree regressor score:', model_r.score(X_test, y_test))
+    model_r = DecisionTreeRegressor()
+    reg = model_r.fit(X_train, y_train)
+    scores = cross_val_score(model_r, X.values, y.values.ravel(), cv=5, scoring='r2')
+
+    print('\nDecision tree regressor score:', model_r.score(X_test, y_test))
+    print('Decision tree cross-validation mean score is: ', scores.mean(), scores.std())
+
+    # plt.figure()
+    # plot_tree(reg, filled=True)
+    # plt.savefig('fig.png', dpi=600)
+    # plt.show()
 
 
-def linear_regression(X_train, X_test, y_train, y_test):
+
+
+def linear_regression(X_train, X_test, y_train, y_test, X, y):
     """
     Sources:
         https://satishgunjal.com/multivariate_lr_scikit/
@@ -51,10 +58,12 @@ def linear_regression(X_train, X_test, y_train, y_test):
     """
     model_r = linear_model.Ridge(alpha=1)
     model_r.fit(X_train, y_train)
-    print('Linear regression score:', model_r.score(X_test, y_test))
+    scores = cross_val_score(model_r, X.values, y.values.ravel(), cv=5, scoring='r2')
+    print('\nLinear regression score:', model_r.score(X_test, y_test))
+    print('Linear regression cross-validation mean score is: ', scores.mean(), scores.std())
 
 
-def non_linear_regression(X_train, X_test, y_train, y_test):
+def non_linear_regression(X_train, X_test, y_train, y_test, X, y):
     """
     Sources:
         https://scikit-learn.org/stable/auto_examples/svm/plot_svm_regression.html
@@ -66,4 +75,6 @@ def non_linear_regression(X_train, X_test, y_train, y_test):
     """
     model_r = SVR()
     model_r.fit(X_train, y_train)
-    print('Non-Linear regression score:', model_r.score(X_test, y_test))
+    scores = cross_val_score(model_r, X.values, y.values.ravel(), cv=5, scoring='r2')
+    print('\nNon-Linear regression score:', model_r.score(X_test, y_test))
+    print('Non-Linear regression cross-validation mean score is: ', scores.mean(), scores.std())
